a613b3a0ad5f36050e8a09afb08cfbb87fc0f161
mscaliza <mariana.scaliza@gmail.com>
2021-05-04 11:49:04 -0300

    ajustando documentação swagger

--- src/main/java/com/pedido/api/model/dto/LancheDTO.java
+++ src/main/java/com/pedido/api/model/dto/LancheDTO.java
@@ -1,5 +1,6 @@
 package com.pedido.api.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -9,14 +10,29 @@ import java.util.List;
 
 @Data
 public class LancheDTO implements Serializable {
+
+    @ApiModelProperty(value = "Código da lanche", required = true)
     private Long id;
+
+    @ApiModelProperty(value ="Nome do lanche", hidden = true)
     private String descricao;
+
+    @ApiModelProperty(value = "Quantidade", required = true)
     private Long quantidade;
+
+    @ApiModelProperty(value = "NORMAL ou PERSONALIZADO", required = true)
     private String tipo;
+
+
     private List<IngredienteDTO> ingredientes;
+
+    @ApiModelProperty(value ="Promoções aplicadas", hidden = true)
     private List<PromocaoDTO> promocao = new ArrayList<>();
 
+    @ApiModelProperty(value ="Valor", hidden = true)
     private BigDecimal valor;
+
+    @ApiModelProperty(value ="Valor promocional", hidden = true)
     private BigDecimal valorPromocional;
 
     public void addPromocao(PromocaoDTO promocao){
